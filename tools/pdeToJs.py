e5e5a466e37dfa9b323b4e8e124d472489ed75d4
haschdl <h.abude@gmail.com>
2019-10-21 23:34:44 +0200

    Port of Week 2

--- tools/pdeToJs.py
+++ tools/pdeToJs.py
@@ -1,62 +1,71 @@
 import re
+import sys
+import argparse
 
-input = "example1.pde"
-output = "example1.js"
+
 
 rgxDraw = [
     re.compile('(.*\.beginDraw\(\)\;)'),
-    re.compile('(.*\.beginDraw\(\)\;)'
+    re.compile('(.*\.beginDraw\(\)\;)')
     ]
 
 rgxMethod = re.compile('[a-zA-Z]*\s*\(.*\)\s*{')
 
 regExLoops = [re.compile('for\s*\('), re.compile('if\s*\(')]
 
-with open(input, 'r') as f, open(output, 'w') as o:
-    code = f.read()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_in', help='input file')
+    parser.add_argument('file_out', help='output file')
+    args = parser.parse_args()
 
-    # replace typed declarations (excluding function)
-    types = ['PGraphics', 'PVector', 'float', 'int']
-    
-    matches = re.findall(rgxMethod, code)
-    for m in matches:
-        text = str(m)
-        if (any (regex.match(text) for regex in regExLoops)):
-            continue
+    with open(args.file_in, 'r') as f, open(args.file_out, 'w') as o:
+        code = f.read()
 
-        print(text)
-        for var in types:
-            text = text.replace(var +  " ", "")
-
-        print(text)
-        code = code.replace(str(m), text)  
+        # replace typed declarations (excluding function)
+        types = ['PGraphics', 'PVector', 'float', 'float[]', 'int', 'int[]']
         
+        matches = re.findall(rgxMethod, code)
+        for m in matches:
+            text = str(m)
+            if (any (regex.match(text) for regex in regExLoops)):
+                continue
 
-    # replace "void" with function
-    code = code.replace('void ', 'function ')
-   
-    for var in types:
-        code = code.replace(var + ' ', 'let ')
+            print(text)
+            for var in types:
+                text = text.replace(var +  " ", "")
 
-    # replace matrix operations
-    code = code.replace('.pushMatrix()', '.push()')
-    code = code.replace('.popMatrix()', '.pop()')
+            print(text)
+            code = code.replace(str(m), text)  
+            
 
-    # replace console operations
-    code = code.replace('println(', 'console.debug(')
+        # replace "void" with function
+        code = code.replace('void ', 'function ')
+    
+        for var in types:
+            code = code.replace(var + ' ', 'let ')
 
-    # remove unnecessary operations
-    # comment out 'beginDraw'. Using RegEx group replacement
-    for r  in rgxDraw:
-        code = re.sub(r, r'/* \1 */', code)
+        # replace matrix operations
+        code = code.replace('.pushMatrix()', '.push()')
+        code = code.replace('.popMatrix()', '.pop()')
+
+        # replace console operations
+        code = code.replace('println(', 'console.debug(')
+
+        # remove unnecessary operations
+        # comment out 'beginDraw'. Using RegEx group replacement
+        for r  in rgxDraw:
+            code = re.sub(r, r'/* \1 */', code)
 
 
-    # random stuff
-    replaces = [ ("Long.signum", "Math.sign"),
-    ("new PVector", "new p5.Vector")]
-    for a,b in replaces:
-        code = code.replace(a,b)
+        # random stuff
+        replaces = [ ("Long.signum", "Math.sign"),
+        ("new PVector", "new p5.Vector")]
+        for a,b in replaces:
+            code = code.replace(a,b)
 
-    #writes output
-    o.writelines(code)
+        #writes output
+        o.writelines(code)
 
+if __name__ == '__main__':
+    main()
