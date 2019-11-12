3a3e3e190fb33328f6abe7db33f02b72b922235d
Régis Frias <regis.frias@motleyagency.com>
2019-11-12 20:49:57 +0200

    Fix sketch paths and remove p5 files.

--- helsinki/Week-17/sketch/sketch.js
+++ helsinki/Week-17/sketch/sketch.js
@@ -9,7 +9,6 @@ function setup() {
   altitude = (side * sqrt(3))/2;
 }
 
-
 function draw() {
   background(255);
 
@@ -23,20 +22,20 @@ function drawHexagon(startAngle, center, _odd){
       const imgFraction = 1; //img.height/2.5;
       const x1 = cos(i - PI/3) * side + center.x;
       const y1 = sin(i - PI/3) * side + center.y;
-      
+
       const x2 = cos(i) * side + center.x;
       const y2 = sin(i) * side + center.y;
-      
+
       const tex1 = odd ? {u: imgFraction, v: imgFraction,} : {u: img.width - imgFraction, v: imgFraction,};
       const tex2 = odd ? {u: img.width - imgFraction, v: imgFraction,} : {u: imgFraction, v: imgFraction,};
-      
+
       texture(img);
       beginShape();
       vertex(x1, y1, tex1.u, tex1.v);
       vertex(x2, y2, tex2.u, tex2.v);
       vertex(center.x, center.y, img.width/2, img.height - imgFraction);
       endShape();
-      
+
       odd = !odd;
     }
   }
